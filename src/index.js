9bdd8334040684ab8ce8ef6aa404734c77ebf7e6
kmakai <makaikwag@gmail.com>
2021-12-25 07:50:20 -0500

    .

--- src/index.js
+++ src/index.js
@@ -3,33 +3,30 @@ import loadMenu from "./menu.js";
 console.log("hello world");
 
 function createNav() {
-    const navigation = document.createElement('nav');
-    const navList = document.createElement('ul');
+  const navigation = document.createElement('nav');
+  const navList = document.createElement('ul');
 
-    const listItem1 = document.createElement('li');
-    listItem1.innerText = "Home"
-    navList.appendChild(listItem1);
-    listItem1.addEventListener("click", () => { loadHome() });
+  const listItem1 = document.createElement('li');
+  listItem1.innerText = "Home"
+  navList.appendChild(listItem1);
+  listItem1.addEventListener("click", () => { loadHome() });
 
 
-    const listItem2 = document.createElement('li');
-    listItem2.innerText = "Menu"
-    navList.appendChild(listItem2);
-    listItem2.addEventListener("click", () => { loadMenu() });
+  const listItem2 = document.createElement('li');
+  listItem2.innerText = "Menu"
+  navList.appendChild(listItem2);
+  listItem2.addEventListener("click", () => { loadMenu() });
 
-    const listItem3 = document.createElement('li');
-    listItem3.innerText = "Contact";
-    navList.appendChild(listItem3);
-    listItem3.addEventListener("click", () => { loadContact() });
+  const listItem3 = document.createElement('li');
+  listItem3.innerText = "Contact";
+  navList.appendChild(listItem3);
+  listItem3.addEventListener("click", () => { loadContact() });
 
-    navigation.appendChild(navList);
-    navigation.classList = "navbar";
+  navigation.appendChild(navList);
+  navigation.classList = "navbar";
 
-    return navigation;
-  }
+  return navigation;
+}
 
-  function loadNav() {
-    document.querySelector("#content").append(createNav());
-  }
 
-loadNav();
+document.querySelector("#content").append(createNav());
